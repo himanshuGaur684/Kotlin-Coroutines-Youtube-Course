fa69a96f856a0f75c56310ca3ac4871bcb67a1a3
Himanshu <himanshugaur684@gmail.com>
2025-03-26 12:31:46 +0530

    Initial Coroutine builders

--- app/src/main/java/dev/himanshu/kotlincoroutinesyoutubecourse/MainActivity.kt
+++ app/src/main/java/dev/himanshu/kotlincoroutinesyoutubecourse/MainActivity.kt
@@ -8,9 +8,6 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.Scaffold
 import androidx.compose.ui.Modifier
 import dev.himanshu.kotlincoroutinesyoutubecourse.ui.theme.KotlinCoroutinesYoutubeCourseTheme
-import kotlinx.coroutines.delay
-import kotlinx.coroutines.launch
-import kotlinx.coroutines.runBlocking
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -24,40 +21,6 @@ class MainActivity : ComponentActivity() {
             }
         }
     }
-}
-
-
-fun main(): Unit = runBlocking {
-
-    launch {
-        takeOrder("Customer 1")
-        cookFood("Customer 1")
-        deliverIt("Customer 1")
-    }
-
-    launch {
-        takeOrder("Customer 2")
-        cookFood("Customer 2")
-        deliverIt("Customer 2")
-    }
-
-
-
-}
-
-suspend fun takeOrder(name: String) {
-    println("Order have been taken for $name")
-}
-
-suspend fun cookFood(name: String) {
-    println("Food is cooked for $name")
-    delay(4000)
-}
-
-
-suspend fun deliverIt(name: String) {
-    println("Deliver it to $name")
-
 }
 
 
